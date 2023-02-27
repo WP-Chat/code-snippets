1a8f8d3f70d3e4f1e5aa4b541121c657fd022d21
Thomas de Visser <thomasdevisser@pm.me>
2023-02-27 10:23:08 +0100

    Added sources

--- javascript/string-utils.js
+++ javascript/string-utils.js
@@ -2,6 +2,7 @@
  * Slugify a string.
  * @param {string} string The string to slugify.
  * @returns {string} The slugified string.
+ * @link https://github.com/tomdevisser/tomdevisser-theme/blob/main/src/js/utils.js
  */
 const slugifyString = (string) => {
 	return string
